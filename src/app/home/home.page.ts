0a724a50e4f021e7fc346711a0b3bf84715088f4
huayyee <a171926@siswa.ukm.edu.my>
2019-10-16 19:20:53 +0800

    first

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router, Route, NavigationExtras } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router, private route: Route, private navigate: NavigationExtras) {}
+
+  submit(){
+
+    console.log("Button ok.");
+    this.router.navigate(['about']);
+  
+  }
+}
